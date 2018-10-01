4a0f9645da3c002dc096d9332f744aadb942bcc2
Rémi Audebert <rflah0@gmail.com>
2018-10-01 23:11:01 +0200

    cellaservctl: add basic request

--- cmd/cellaservctl/main.go
+++ cmd/cellaservctl/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/evolutek/cellaserv3/client"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	request     = kingpin.Command("request", "Make a request to a service.")
+	requestPath = request.Arg("path", "request path: service.method, service/id.method or service[id].method").Required().String()
+	requestArgs = request.Arg("args", "args for the method").String()
+)
 
 func main() {
-	fmt.Println("vim-go")
+	switch kingpin.Parse() {
+	case "request":
+		client := client.NewConnection(":4200")
+		// Parse service and method
+		requestPathSlice := strings.Split(*requestPath, ".")
+		requestService := requestPathSlice[0]
+		requestMethod := requestPathSlice[1]
+		service := client.NewServiceStub(requestService, "")
+
+		// Serialize request args
+		var requestPayload interface{}
+		json.Unmarshal([]byte(*requestArgs), &requestPayload)
+
+		// Make request
+		respBytes := service.Request(requestMethod, requestPayload)
+
+		// Display response
+		var requestResponse interface{}
+		json.Unmarshal(respBytes, &requestResponse)
+		fmt.Printf("%#v\n", requestResponse)
+	}
 }
