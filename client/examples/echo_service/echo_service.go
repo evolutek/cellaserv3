b968a7af64cbf466d4291a46d1901541415307d7
Rémi Audebert <rflah0@gmail.com>
2018-10-01 23:35:47 +0200

    Add echo_service

---
+++ client/examples/echo_service/echo_service.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+
+	cellaserv "bitbucket.org/evolutek/cellaserv2-protobuf"
+	"github.com/evolutek/cellaserv3/client"
+)
+
+func main() {
+	// Connect to cellaserv
+	conn := client.NewConnection(":4200")
+
+	// Prepare service for registration
+	echo := conn.NewService("echo", "")
+	// Handle "time" request
+	echo.HandleRequestFunc("echo", func(req *cellaserv.Request) (interface{}, error) {
+		// Parse json from request
+		var reqObj interface{}
+		err := json.Unmarshal(req.GetData(), &reqObj)
+		return reqObj, err
+	})
+	// Handle "killall" event
+	echo.HandleEventFunc("killall", func(_ *cellaserv.Publish) {
+		conn.Close()
+	})
+
+	// Register the service on cellaserv
+	conn.RegisterService(echo)
+
+	conn.WaitClose()
+}
