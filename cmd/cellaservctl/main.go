70c2c578424e1f63240a3fc30533b3b4b01715d4
Rémi Audebert <rflah0@gmail.com>
2018-10-01 23:21:29 +0200

    cellaservctl: request: add support for identification and KEY=VALUE args

--- cmd/cellaservctl/main.go
+++ cmd/cellaservctl/main.go
@@ -11,27 +11,37 @@ import (
 )
 
 var (
-	request     = kingpin.Command("request", "Make a request to a service.")
-	requestPath = request.Arg("path", "request path: service.method, service/id.method or service[id].method").Required().String()
-	requestArgs = request.Arg("args", "args for the method").String()
+	request     = kingpin.Command("request", "Make a request to a service.").Alias("r")
+	requestPath = request.Arg("path", "request path: service.method, service/id.method").Required().String()
+	requestArgs = request.Arg("args", "args for the method").StringMap()
 )
 
 func main() {
 	switch kingpin.Parse() {
 	case "request":
+		// Connect to cellaserv
 		client := client.NewConnection(":4200")
+
 		// Parse service and method
 		requestPathSlice := strings.Split(*requestPath, ".")
 		requestService := requestPathSlice[0]
 		requestMethod := requestPathSlice[1]
-		service := client.NewServiceStub(requestService, "")
 
-		// Serialize request args
-		var requestPayload interface{}
-		json.Unmarshal([]byte(*requestArgs), &requestPayload)
+		// Check for identification
+		var requestServiceIdentification string
+		requestMaybeIdentificationSlice := strings.Split(requestService, "/")
+
+		if len(requestMaybeIdentificationSlice) == 2 {
+			// The user wrote service/id.method
+			requestService = requestMaybeIdentificationSlice[0]
+			requestServiceIdentification = requestMaybeIdentificationSlice[1]
+		}
+
+		// Create service stub
+		service := client.NewServiceStub(requestService, requestServiceIdentification)
 
 		// Make request
-		respBytes := service.Request(requestMethod, requestPayload)
+		respBytes := service.Request(requestMethod, requestArgs)
 
 		// Display response
 		var requestResponse interface{}
